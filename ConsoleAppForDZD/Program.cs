ee6ec88ff0032b67d1c250c7128709b0e8947a98
DaViD DvALi <DaViD DvALi>
2018-11-18 16:28:22 +0400

    Add project files.

---
+++ ConsoleAppForDZD/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+//using System.Collections.Generic
+using System.Text;
+using System.Threading.Tasks;
+using DZD.Linq;
+//using System.Linq;
+
+namespace ConsoleAppForDZD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] arr = new int[10] { 1, 2, 3, 4, 11, 34, 33, 1, 13, 5 };
+
+            //var js = from ar in arr
+            //         where ar > 5
+            //         select ar * 2;
+
+            //foreach (var k in js)
+            //    Console.WriteLine(k);
+
+            //Console.WriteLine(arr.Where((k, i) => k > 5));
+
+            //1)Implementing Empty
+            //var k = Enumarable.Empty<int>();
+            //var l = k.ToList();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DZD.Linq/Enumarable.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZD.Linq
+{
+    public static partial class Enumarable
+    {
+        public static IEnumerable<TDZD> Where<TDZD>(this IEnumerable<TDZD> list, Func<TDZD, bool> func)
+        {
+            if (list == null)
+                throw new ArgumentNullException("list");
+            if (func == null)
+                throw new ArgumentNullException("func");
+
+            return WhereImpl(list, func);
+        }
+
+        private static IEnumerable<TDZD> WhereImpl<TDZD>(IEnumerable<TDZD> list, Func<TDZD, bool> func)
+        {
+            foreach (var l in list)
+                if (func(l))
+                    yield return l;
+        }
+
+        public static IEnumerable<TDZD> Where<TDZD>(this IEnumerable<TDZD> list, Func<TDZD, int, bool> func)
+        {
+            if (list == null)
+                throw new ArgumentNullException("list");
+            if (func == null)
+                throw new ArgumentNullException("func");
+
+            return WhereImpl(list, func);
+        }
+
+        private static IEnumerable<TDZD> WhereImpl<TDZD>(IEnumerable<TDZD> list, Func<TDZD, int, bool> func)
+        {
+            var count = 0;
+            foreach (var l in list)
+            {
+                if (func(l, count))
+                    yield return l;
+                count++;
+            }
+        }
+
+        public static IEnumerable<TRes> Select<TDZD, TRes>(this IEnumerable<TDZD> list, Func<TDZD, TRes> func)
+        {
+            if (list == null)
+                throw new ArgumentNullException("list");
+            if (func == null)
+                throw new ArgumentNullException("func");
+
+            return SelectImpl(list, func);
+        }
+
+        private static IEnumerable<TRes> SelectImpl<TDZD, TRes>(IEnumerable<TDZD> list, Func<TDZD, TRes> func)
+        {
+            foreach (var l in list)
+                yield return func(l);
+        }
+
+        public static IEnumerable<TRes> Select<TDZD, TRes>(this IEnumerable<TDZD> list, Func<TDZD, int, TRes> func)
+        {
+            if (list == null)
+                throw new ArgumentNullException("list");
+            if (func == null)
+                throw new ArgumentNullException("func");
+
+            return SelectImpl(list, func);
+        }
+
+        private static IEnumerable<TRes> SelectImpl<TDZD, TRes>(IEnumerable<TDZD> list, Func<TDZD, int, TRes> func)
+        {
+            int count = 0;
+            foreach (var l in list)
+            {
+                yield return func(l, count);
+                count++;
+            }
+        }
+
+        public static IEnumerable<int> Range(int start, int count)
+        {
+            if (count < 0)
+                throw new ArgumentOutOfRangeException("count");
+
+            //This is also long and do not need casts
+            //if(start + 1L + count)
+            if (start + (long)count - 1L > int.MaxValue)
+                throw new ArgumentOutOfRangeException("count");
+
+            return RangeImpl(start, count);
+        }
+
+        private static IEnumerable<int> RangeImpl(int start, int count)
+        {
+            for (int temp = start; temp < start + count; temp++)
+                yield return temp;
+        }
+
+        public static IEnumerable<DZD> Empty<DZD>()
+        {
+            //It is cached for each DZD type. two DZD types access equal references
+            return EmptyHolder<DZD>.Array;
+        }
+
+        private static class EmptyHolder<T>
+        {
+            internal static readonly T[] Array = new T[0];
+        }
+    }
+}
