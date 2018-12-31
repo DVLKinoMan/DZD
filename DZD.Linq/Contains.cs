b216364837bd605d4eb0e91426c582ee77e31cb8
david.dvali <david.dvali@DAVID-DVALI>
2018-12-31 17:49:51 +0400

    Implement Contains

---
+++ DZD.Linq/Contains.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DZD.Linq
+{
+    public partial class Enumerable
+    {
+        public static bool Contains<TSource>(this IEnumerable<TSource> source, TSource value, IEqualityComparer<TSource> comparer)
+        {
+            comparer = comparer ?? EqualityComparer<TSource>.Default;
+            return source.Any(item => comparer.Equals(value, item));
+        }
+    }
+}
